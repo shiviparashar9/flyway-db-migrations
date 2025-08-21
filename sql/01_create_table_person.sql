a75d65f0e881a33c47484f0c1a32847e07d87951
shiviparashar9 <shiviparashar8007@gmail.com>
2025-08-21 12:01:52 +0530

    Delete sql/01_create_table_person.sql

--- sql/01_create_table_person.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ntnxschema1.person (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
-);
-
-INSERT INTO ntnxschema1.person(name, email)
-select 'test','test@email.com';
